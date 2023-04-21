81c45206e556b1446a15701bf0f628671b396dfc
Ivan Komarov <ikomarov@kse.org.ua>
2023-04-21 17:08:10 +0300

    Initial Commit

---
+++ DebtList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DebtList
+//
+//  Created by Ivan Komarov on 21.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
